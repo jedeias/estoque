9c2b28c4939a9e92b17e0b3457a4771a1fd2fe4c
wendell <wendellparanhos326@gmail.com>
2024-02-20 11:42:25 -0300

    Arrumado bug das tabelas

--- view/php/products.php
+++ view/php/products.php
@@ -23,8 +23,7 @@ $user = unserialize($serializedUser);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/user.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="../js/products.js"></script>
+    
     <title>User</title>
 </head>
 <body>
@@ -94,6 +93,10 @@ $user = unserialize($serializedUser);
       <p>Nome: <?php echo $user->getName();?></p>
       <p>Email: <?php echo $user->getEmail();?></p>
    </div>
+
+
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="../js/products.js"></script>
 
 </body>
 </html>
